af62d384b6d759e075473e348c5a4e22b6ae592f
qiangge <qiangge@meili-inc.com>
2017-07-08 23:14:46 +0800

    添加远程selenium远程代理

--- remote.py
+++ remote.py
@@ -2,6 +2,7 @@
 # coding:  utf-8
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
 
 selenium_grid_url = "http://0.0.0.0:4444/wd/hub"
 
@@ -12,3 +13,6 @@ capabilities['version'] = "59.0.3071.115"
 
 # Instantiate an instance of Remote WebDriver with the desired capabilities.
 driver = webdriver.Remote(desired_capabilities=capabilities, command_executor=selenium_grid_url)
+driver.get("http://www.baidu.com")
+driver.find_element_by_name("wd").send_keys(u"蘑菇街")
+driver.find_element_by_name("wd").send_keys(Keys.ENTER)
