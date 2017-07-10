142dc4be9237a588a3a0d40dc4866f202a9f71d3
qiangge <qiangge@meili-inc.com>
2017-07-10 22:06:03 +0800

    添加测试

---
+++ scrapytest/scrapytest/spiders/tmail.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class TmailSpider(scrapy.Spider):
+    name = 'tmail'
+    allowed_domains = ['www.tmail.com', 'detail.tmail.com']
+    start_urls = ['http://www.tmail.com/']
+
+    def parse(self, response):
+        pass
